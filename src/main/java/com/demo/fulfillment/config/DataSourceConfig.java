a364e26a70f1c3bbb981e6caea979999e6223014
Gabriel Enslein <genslein16@gmail.com>
2022-02-13 15:32:23 -0500

    add initial migration, datasource changes for flyway

--- src/main/java/com/demo/fulfillment/config/DataSourceConfig.java
+++ src/main/java/com/demo/fulfillment/config/DataSourceConfig.java
@@ -2,14 +2,9 @@ package com.demo.fulfillment.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import io.micrometer.core.instrument.MeterRegistry;
-//import org.flywaydb.core.Flyway;
-//import org.flywaydb.core.api.MigrationVersion;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
@@ -20,24 +15,8 @@ public class DataSourceConfig {
     @Value("${JDBC_DATABASE_URL}")
     public String JDBC_DATABASE_URL;
 
-    // Default to false for environment deployments to not auto-migrate
-    // set to true for local test suite
-    @Value("${spring.flyway.enabled:false}")
-    public Boolean FLYWAY_ENABLED;
-
-    @Value("${flyway.createSchemas:true}")
-    public Boolean CREATE_SCHEMAS_FLAG;
-
-    @Value("${flyway.defaultSchema:public}")
-    public String DEFAULT_SCHEMA;
-
     @Value("${logging.level.root:}")
     public String LOG_SQL;
-
-    // comma-separated list of names, can be asked to separate schemas.
-    // Not Recommended to use multiple schemas without a specific business need
-    @Value("${flyway.schemas:public}")
-    public String SCHEMAS_NEEDED;
 
     @Bean
     public DataSource dataSource() {
@@ -51,19 +30,4 @@ public class DataSourceConfig {
 
         return new HikariDataSource(config);
     }
-
-//    @Bean(initMethod = "migrate")
-//    @DependsOn({"dataSource"})
-//    Flyway flyway(DataSource dataSource) {
-//        Flyway flyway = Flyway.configure()
-//                .dataSource(dataSource)
-//                .defaultSchema(DEFAULT_SCHEMA)
-//                .createSchemas(CREATE_SCHEMAS_FLAG)
-//                .schemas(SCHEMAS_NEEDED)
-//                .baselineOnMigrate(true)
-//                .target(MigrationVersion.LATEST)
-//                .load();
-//
-//        return flyway;
-//    }
 }
--- src/main/java/com/demo/fulfillment/config/GeneralDataConfig.java
+++ src/main/java/com/demo/fulfillment/config/GeneralDataConfig.java
@@ -3,8 +3,11 @@ package com.demo.fulfillment.config;
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.config.MeterFilter;
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.MigrationVersion;
 import org.hibernate.cfg.Environment;
 import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
@@ -23,10 +26,26 @@ import java.util.Properties;
 @EnableTransactionManagement
 @EnableJpaRepositories("com.demo.fulfillment.repositories")
 public class GeneralDataConfig {
+    // Default to false for environment deployments to not auto-migrate
+    // set to true for local test suite
+    @Value("${spring.flyway.enabled:false}")
+    public Boolean FLYWAY_ENABLED;
+
+    @Value("${flyway.createSchemas:true}")
+    public Boolean CREATE_SCHEMAS_FLAG;
+
+    @Value("${flyway.defaultSchema:public}")
+    public String DEFAULT_SCHEMA;
+
+    // comma-separated list of names, can be asked to separate schemas.
+    // Not Recommended to use multiple schemas without a specific business need
+    @Value("${flyway.schemas:public}")
+    public String SCHEMAS_NEEDED;
+
     final Properties additionalProperties() {
         final Properties hibernateProperties = new Properties();
         hibernateProperties.put(Environment.GENERATE_STATISTICS, true);
-        hibernateProperties.setProperty(Environment.HBM2DDL_AUTO, "update");
+        hibernateProperties.setProperty(Environment.HBM2DDL_AUTO, "validate");
         hibernateProperties.setProperty(Environment.DIALECT, org.hibernate.dialect.PostgreSQL10Dialect.class.getCanonicalName());
         hibernateProperties.setProperty(Environment.SHOW_SQL, "true");
         hibernateProperties.setProperty(Environment.FORMAT_SQL, "true");
@@ -62,6 +81,21 @@ public class GeneralDataConfig {
     @Bean
     public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
         return new PersistenceExceptionTranslationPostProcessor();
+    }
+
+    @Bean(initMethod = "migrate")
+    @DependsOn({"dataSource"})
+    Flyway flyway(DataSource dataSource) {
+        Flyway flyway = Flyway.configure()
+                .dataSource(dataSource)
+                .defaultSchema(DEFAULT_SCHEMA)
+                .createSchemas(CREATE_SCHEMAS_FLAG)
+                .schemas(SCHEMAS_NEEDED)
+                .baselineOnMigrate(true)
+                .target(MigrationVersion.LATEST)
+                .load();
+
+        return flyway;
     }
 
     @Bean
